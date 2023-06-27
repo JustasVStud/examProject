a98adfabfa1196c22d44b3c74cef32460736f8ca
Justas Vaitkevicius <Justas.Vaitkevicius@stud.vtmc.lt>
2023-06-27 13:19:55 +0300

    fixed employee edit

--- exam.front/src/components/autoServices/EmployeeEdit.jsx
+++ exam.front/src/components/autoServices/EmployeeEdit.jsx
@@ -3,10 +3,14 @@ import { Formik } from 'formik';
 import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import * as Yup from 'yup';
-import { getEmployee, editEmployee } from '../service/autoService.service';
+import { getEmployee, editEmployee, getAutoServices } from '../service/autoService.service';
 
 const employeeValidationSchema = Yup.object().shape({
-  title: Yup.string().required('View Item title is required'),
+  name: Yup.string().required('Employee name is required'),
+  surname: Yup.string().required('Employee surname is required'),
+  specialty: Yup.string().required('Employee specialty is required'),
+  city: Yup.string().required('Employee city is required'),
+  autoServiceId: Yup.string().required('Employee place of work is required'),
 });
 
 function EmployeeEdit() {
@@ -14,6 +18,7 @@ function EmployeeEdit() {
   const [errorMessage, setErrorMessage] = useState('');
   const [showError, setShowError] = useState(false);
   const [employee, setEmployee] = useState([]);
+  const [autoServices, setAutoServices] = useState([]);
   const {id} = useParams();
 
   useEffect(() => {
@@ -31,6 +36,18 @@ function EmployeeEdit() {
         fetchEmployee(id);
     }
   }, [id]);
+  useEffect(() => {
+    const fetchAutoServices = async () => {
+      try {
+        const autoServicesData = await getAutoServices();
+        setAutoServices(autoServicesData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    fetchAutoServices();
+  }, []);
 
   const handleEmployeeUpdate = async (values, {resetForm}) => {
     try {
@@ -61,7 +78,11 @@ function EmployeeEdit() {
       <Row>
         <Formik
           initialValues={{
-            title: employee.title,
+            name: employee.name,
+            surname: employee.surname,
+            specialty: employee.specialty,
+            city: employee.specialty,
+            autoServiceId: employee.autoServiceId
           }}
           validationSchema={employeeValidationSchema}
           onSubmit={(values, { resetForm }) => {
@@ -80,17 +101,80 @@ function EmployeeEdit() {
           }) => (
             <Form onSubmit={handleSubmit}>
               <Form.Group className="mb-3">
-                <Form.Label>Title</Form.Label>
+                <Form.Label>Name</Form.Label>
                 <Form.Control
                   type="text"
-                  name="title"
+                  name="name"
                   size="sm"
-                  value={values.title}
+                  value={values.name}
                   onChange={handleChange}
                   onBlur={handleBlur}
-                  isInvalid={touched.title && !!errors.title}
+                  isInvalid={touched.name && !!errors.name}
                 />
-                <Form.Control.Feedback type="invalid">{errors.title}</Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Surname</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="surname"
+                  size="sm"
+                  value={values.surname}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  isInvalid={touched.surname && !!errors.surname}
+                />
+                <Form.Control.Feedback type="invalid">{errors.surname}</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Speacialty</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="specialty"
+                  size="sm"
+                  value={values.specialty}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  isInvalid={touched.specialty && !!errors.specialty}
+                />
+                <Form.Control.Feedback type="invalid">{errors.specialty}</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="city"
+                  size="sm"
+                  value={values.city}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  isInvalid={touched.city && !!errors.city}
+                />
+                <Form.Control.Feedback type="invalid">{errors.city}</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Auto Service</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="autoServiceId"
+                  size="sm"
+                  value={values.autoServiceId}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  isInvalid={touched.autoServiceId && !!errors.autoServiceId}
+                >
+                  <option value="">Select auto service</option>
+
+                  {autoServices.map((autoService) => (
+                    <option
+                      key={autoService.id}
+                      value={autoService.id}
+                    >
+                      {autoService.title}
+                    </option>
+                  ))}
+                </Form.Control>
+                <Form.Control.Feedback type="invalid">{errors.autoServiceId}</Form.Control.Feedback>
               </Form.Group>
               <Row className="form-buttons-container">
                 <Col>
